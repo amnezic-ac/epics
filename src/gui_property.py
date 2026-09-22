bd26633f26951384fa8b842017e6077d8d8d881b
amnezic <antoine.leveque@epita.fr>
2026-09-22 18:34:34 +0200

    fix (gui): corection of agencement of attachment labels and entries

--- src/gui_property.py
+++ src/gui_property.py
@@ -2,16 +2,16 @@ from properties import *
 from tkinter import *
 from components.component import Component
 
-def make_attachement_frame(master_frame, component: Component):
+def make_attachement_frame(master_frame):
     attachement_frame = Frame(master_frame)
 
     ### Attachment value
     value_frame = Frame(attachement_frame)
-    value_label = Label(value_frame, text="value")
+    value_label = Label(value_frame, text="value : ")
     value_entry = Entry(value_frame)
-    value_label.pack(side=LEFT)
-    value_entry.pack(side=RIGHT)
-    value_frame.pack(anchor="w")
+    value_label.pack(side="left", anchor="w")
+    value_entry.pack(side=RIGHT, anchor="e")
+    value_frame.pack()
 
     ### If there is an encoding
     encoding_frame = Frame(attachement_frame, padx=1, pady=1)
@@ -23,18 +23,20 @@ def make_attachement_frame(master_frame, component: Component):
     def toggle_encoding_entry_frame():
         test_bool = isEncodingCheckBoxChecked.get()
         if isEncodingCheckBoxChecked.get():
-            encoding_checkbox.pack(side=LEFT)
+            encoding_checkbox.pack(side=LEFT, anchor="w")
             if (not isAlreadyAnEncodingValue.get()):
+                encoding_checkbox.config(text="Encoding : ")
                 entry = Entry(encoding_entry_frame, textvariable=encodingEntryValue)
-                entry.pack()
+                entry.pack(side="right", anchor="e")
                 isAlreadyAnEncodingValue.set(True)
-            encoding_entry_frame.pack(side="right")
+            encoding_entry_frame.pack()
         else:
+            encoding_checkbox.config(text="Encoding ? ")
             encoding_entry_frame.pack_forget()
 
     encoding_checkbox = Checkbutton(encoding_frame, text="Encoding ? ", variable=isEncodingCheckBoxChecked, onvalue=True, offvalue=False, command=toggle_encoding_entry_frame)
-    encoding_checkbox.pack()
-    encoding_frame.pack(anchor="w")
+    encoding_checkbox.pack(side="left", anchor="w")
+    encoding_frame.pack()
     
     ### If there is (sub)type
     type_frame = Frame(attachement_frame)
@@ -48,17 +50,19 @@ def make_attachement_frame(master_frame, component: Component):
         if isTypeCheckBoxchecked.get():
             type_checkbox.pack(side="left")
             if (not isAlreadyTypeValue.get()):
-                Entry(typesEntryFrame, textvariable=typeEntryValue).pack(side=RIGHT)
+                type_checkbox.config(text="type/subtype : ")
+                Entry(typesEntryFrame, textvariable=typeEntryValue).pack(side=RIGHT, anchor="e")
                 Label(typesEntryFrame, text="/").pack(side=RIGHT)
                 Entry(typesEntryFrame, textvariable=subtypeEntryValue).pack(side=RIGHT)
                 isAlreadyTypeValue.set(True)
-            typesEntryFrame.pack(side="right")
+            typesEntryFrame.pack()
         else:
+            type_checkbox.config(text="Type ? ")
             typesEntryFrame.pack_forget()
 
     type_checkbox = Checkbutton(type_frame, text="Type ? ", variable=isTypeCheckBoxchecked, onvalue=True, offvalue=False, command=toggle_type_entry_frame)
-    type_checkbox.pack()
-    type_frame.pack(anchor="w")
+    type_checkbox.pack(side="left", anchor="w")
+    type_frame.pack()
 
     def submit_attachement():
         value_error_label = Label(value_frame, text="Unable to add an empty value, please put a value or unchecked the encoding checkbox")
@@ -71,7 +75,6 @@ def make_attachement_frame(master_frame, component: Component):
         subtypename = subtypeEntryValue.get() if subtypeEntryValue.get() != "" else None
         encoding = encodingEntryValue.get()
         attachement = Attachement(value_entry.get(), encoding=encoding, typename=typename, subtypename=subtypename)
-        component.attachement = attachement
 
     Button(attachement_frame, text="Submit", command=submit_attachement).pack()
 
